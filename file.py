3833e34bf7cc6da2e35fd06e1be2518fc4e9cb6f
Jarjunior06 <152203081+Jarjunior06@users.noreply.github.com>
2023-12-11 10:12:47 -0700

    Update file.py

--- file.py
+++ file.py
@@ -10,40 +10,46 @@ def choose_word():
 def display_word(word, guessed_letters):
   display = ""
   for letters in word: 
-  if letters in guessed_letters:
-    display += letter
+    if letters in guessed_letters:
+      display += letters
   else:
     display += "_"
     return display
 
 def wordle():
-  max attempts = 6
-  guessed_letters + []
-  word_to_guess = choose_word()
+  max_attempt = 6
+  guessed_letters = []
+  attempt=0
+  secret_word = choose_word()
 
-print("Welcome to Wordle!")
+  print("Welcome to Wordle!")
+  print(display_word(secret_word, guessed_letters))
+  
+  while attempt < max_attempt:
+      guess=input("Enter your guess!:").lower()
+      if len(guess)==1 and guess.isalpha():
+          if guess in guessed_letters:
+                        print("you already guessed that letter. try again")
+          elif guess in secret_word:
+            print("corret!")
 
-for attempt in range(max_attempts):
-  print("\nAttempt", attempt + 1)
-  current_display =
-display_word(word_to_guess, guessed_letters)
-print("Current Word:",,
-current_display)
+      guessed_letters.append(guess)
+  else:
+      print("Inncorect!")
+      attempt +=1
 
-guess = input("Enter your guess:").lower()
+      print(display_word(secret_word, guessed_letters))
 
-      if len(guess) == 1 and 
-guess.isalpha():
-   guessed_letters.append(guess)
 
-if set(guessed_letters) >=
-set(word_to_guess):
-       print("Congratulations! You guessed the correct word:", word_to_guess)
-  break
-else:
-print("Invalid input. Please enter another single letter.")
+  if set(guessed_letters) == set(secret_word):
+    print("congrautulations! you guessed the word!:", secret_word)
+    
 
-else:
-  print("Sorry, you ran out of attempts. The correct word was:", word_to_guess)
-  if __name__ == "__main__":
-    wordle()
+    print("attempts left:", max_attempt - attempt)
+  else:
+    print("invaild input. plesae enter a single letter")
+
+    if attempt == max_attempt:
+      print("sorry your out of attempts. the word was:", secret_word)
+
+wordle()
